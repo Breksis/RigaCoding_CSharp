a9f19e753de9fe3b27a7b7c7a2fb32bcae5e941a
Juris <47427449+Breksis@users.noreply.github.com>
2020-01-27 18:04:51 +0200

    Project created

---
+++ Day1_basics/Day1_basics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day1_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); //Parasta izvade
+
+            String piemers = "Piemera teikums";
+            Console.WriteLine(piemers);
+
+            piemers = Console.ReadLine();
+            Console.WriteLine(piemers);
+        }
+    }
+}
