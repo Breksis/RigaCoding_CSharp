c35442d365172c94fb14be08e4329e3e2343f476
Juris <47427449+Breksis@users.noreply.github.com>
2020-01-29 20:35:07 +0200

    MajasDarbs1

---
+++ MajasDarbs1/MajasDarbs1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MajasDarbs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet virkni ar vismaz 3 simboliem!");
+            String ievade = Console.ReadLine();
+
+            int garums = ievade.Length;
+
+            if ((garums >= 3) && (ievade == "aaa"))
+            {
+                Console.WriteLine("Jusu ievadita virkne ir pareiza");
+            }
+            else
+            {
+                Console.WriteLine("Jusu ievaditais " + ievade + " ir " + garums + " simbolus garsh, un neatbilst nosacijumiem!!!");
+            }
+
+        }
+    }
+}
