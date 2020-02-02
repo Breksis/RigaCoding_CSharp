69e6bd89f69325d3de4baf247b95aee10e37fcd1
Juris <47427449+Breksis@users.noreply.github.com>
2020-02-02 22:10:23 +0200

    done

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalkulators");
+            
+            Zvaigznes();
+            Console.ReadLine();
+            Menu();
+        }
+
+        static double userInput()
+        {
+            String ievade = Console.ReadLine();
+            double input = Convert.ToDouble(ievade);
+            return input;
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Izvelieties darbibu!");
+            Console.WriteLine("1: Pamatdarbibas(+, -, *, /)");
+            Console.WriteLine("2: Kapinasana");
+            Console.WriteLine("3: Kvadratsakne");
+
+            Zvaigznes();
+
+            String izvele = Console.ReadLine();
+
+            if (izvele == "1")
+            {
+                Pamatdarbibas();
+            }
+            else if (izvele == "2")
+            {
+                Pakapes();
+            }
+            else if (izvele == "3")
+            {
+                Kvadratsaknes();
+            }
+            else Console.WriteLine("Tada izvele neeksiste!");
+        }
+
+        static void Pamatdarbibas()
+        {
+            Console.WriteLine("Ievadiet pirmo skaitli!");
+            double num1 = userInput();
+            Console.WriteLine("Ievadiet otro skaitli!");
+            double num2 = userInput();
+            Console.WriteLine("Ievadiet veicamo darbibu! (+, -, *, /)");
+            String darbiba = Console.ReadLine();
+
+            if (darbiba == "+")
+            {
+                Console.WriteLine("Rezultats: " + num1 + darbiba + num2 + "=" + (num1 + num2));
+            }
+            else if (darbiba == "-")
+            {
+                Console.WriteLine("Rezultats: " + num1 + darbiba + num2 + "=" + (num1 - num2));
+            }
+            else if (darbiba == "*")
+            {
+                Console.WriteLine("Rezultats: " + num1 + darbiba + num2 + "=" + (num1 * num2));
+            }
+            else if (darbiba == "/")
+            {
+                Console.WriteLine("Rezultats: " + num1 + darbiba + num2 + "=" + (num1 / num2));
+            }
+            else Console.WriteLine("Ievadita nepareiza darbiba!");
+        }
+
+        static void Pakapes()
+        {
+            Console.WriteLine("Ievadiet kapinamo skaitli!");
+            double num = userInput();
+            Console.WriteLine("Ievadiet kapinataju!");
+            double pakape = userInput();
+            Console.WriteLine("Skaitlis " + num + " pakape " + pakape + " ir " + Math.Pow(num, pakape));
+        }
+
+        static void Kvadratsaknes()
+        {
+            Console.WriteLine("Ievadiet skaitli, no kura velaties izvilkt kvadratsakni!");
+            double num = userInput();
+            Console.WriteLine("Kvadratsakne no " + num + " ir " + Math.Sqrt(num));
+        }
+
+        static void Zvaigznes(int zv = 50)
+        {
+            int i;
+            for (i = 1; i < zv; i++) Console.Write("*");
+        }
+    }
+}
---
+++ HomeWorkWeek1/HomeWorkWeek1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace HomeWorkWeek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Pozitivs/negativs!");
+            Console.WriteLine("2. Random diena!");
+            Console.WriteLine("3. Paris/neparis!");
+
+            Console.WriteLine("Ievadiet opciju!");
+
+            String izvele = Console.ReadLine();
+
+            if (izvele == "1")
+            {
+                PozNeg();
+            }
+            else if (izvele == "2")
+            {
+                RandomDiena();
+            }
+            else if (izvele == "3")
+            {
+                ParisNeparis();
+            }
+            else Console.WriteLine("Tada izvele neeksiste!");
+        }
+        static int Ievade()
+        {
+            Console.WriteLine("Ievadiet skaitli!");
+            String skaitlaIevade = Console.ReadLine();
+            int skaitlis = Convert.ToInt32(skaitlaIevade);
+
+            return skaitlis;
+        }
+
+        static void PozNeg()
+        {
+            int a = Ievade();
+            if (a > 0)
+            {
+                Console.WriteLine("Skaitlis ir pozitivs");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine("Skaitlis ir negativs");
+            }
+            else Console.WriteLine("Skaitlis ir 0");
+        }
+
+        static void RandomDiena()
+        {
+            Random randDay = new Random();
+            int day = randDay.Next(1, 7);
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Pirmdiena");
+                    break;
+                case 2:
+                    Console.WriteLine("Otrdiena");
+                    break;
+                case 3:
+                    Console.WriteLine("Tresdiena");
+                    break;
+                case 4:
+                    Console.WriteLine("Ceturtdiena");
+                    break;
+                case 5:
+                    Console.WriteLine("Piektdiena");
+                    break;
+                case 6:
+                    Console.WriteLine("Sestdiena");
+                    break;
+                case 7:
+                    Console.WriteLine("Svetdiena");
+                    break;
+            }
+        }
+        static void ParisNeparis()
+        {
+            int oddEven = Ievade();
+
+            if (oddEven % 2 == 0)
+            {
+                Console.WriteLine("Skaitlis ir pozitivs!");
+            }
+            else Console.WriteLine("Skaitlis ir negativs!");
+        }
+    }
+}
