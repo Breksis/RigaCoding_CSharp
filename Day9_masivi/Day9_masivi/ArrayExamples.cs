72c062d1d58f869ff50d53367d1ce86ddb94ec1f
Juris <47427449+Breksis@users.noreply.github.com>
2020-02-19 18:03:41 +0200

    Day9_masivi

---
+++ Day9_masivi/Day9_masivi/ArrayExamples.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_masivi
+{
+    public class ArrayExamples
+    {
+        public ArrayExamples()
+        {
+
+        }
+
+        public static void ex1()
+        {
+            int[] a = new int [5];
+
+            a[0] = 4;
+            a[1] = 88;
+            a[2] = 93;
+            a[3] = 1;
+            a[4] = 6;
+
+            int[] c = { 4, 88, 93, 1, 6 };
+
+            string[] b = new string[3];
+            b[0] = "abc";
+            b[1] = "Hello world";
+            b[2] = "Wow, massivi!";
+
+            Console.WriteLine(a[0]);
+            Console.WriteLine(b[2]);
+
+            for(int i  = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+                a[i] = 0;
+            }
+
+        }
+    }
+}
---
+++ Day9_masivi/Day9_masivi/tasks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_masivi
+{
+    public class tasks
+    {
+        public tasks()
+        {
+
+        }
+
+        //Uztaisit masivu, kuram garumu ievada lietottajs
+        //Pec tam, lai lietotajs aizpilda masivu ar izveletajam vertibam
+        //aizpildisanai izmantojam for ciklu
+        //izvadat masivu
+
+
+        //uztaisit masivu ar 5 elemetiem
+        //string tipa masivs
+        //pirmos 4 elemetus aizpilda cilveks
+        //piektaja elementa glabajas pirmo 4 elementu summa
+
+
+        public static void task1()
+        {
+            Console.WriteLine("Izvelieties masiva garumu.");
+            int garums = Convert.ToInt32(Console.ReadLine());
+
+            int[] mas = new int[garums];
+
+            for(int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine("Ievadiet " + (i+1) + ". elementu.");
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
+
+        }
+
+        public static void task2()
+        {
+            String[] masivs = new string[5];
+
+            for(int i = 0; i < masivs.Length - 1; i++)
+            {
+                Console.WriteLine("Ievadiet simbolu virkni.");
+                masivs[i] = Console.ReadLine();
+                masivs[masivs.Length - 1] += masivs[i];
+            }
+
+            for (int i = 0; i < masivs.Length; i++)
+            {
+                Console.WriteLine(masivs[i]);
+            }
+
+
+        }
+    }
+}
