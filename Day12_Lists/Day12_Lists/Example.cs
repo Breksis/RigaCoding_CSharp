4adecda7342256b3eb4f7a351609435a63d15a92
Juris <47427449+Breksis@users.noreply.github.com>
2020-03-02 17:59:15 +0200

    Listi

---
+++ Day12_Lists/Day12_Lists/Example.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day12_Lists
+{
+    class Example
+    {
+        public static void ex1()
+        {
+            List<int> a = new List<int>();
+            a.Add(2);
+            a.Add(5);
+
+            for(int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+            foreach(int b in a)
+            {
+                Console.WriteLine(b);
+            }
+
+            
+        }
+
+        //Lists ar int skaitliem
+        //Cilveks izvelas, cik skaitlus sarakastam pievienot
+        //Cilveks pievieno skaitlus
+        //Izvada skaitlu summu
+
+        public static void task1()
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Cik skaitlus pievienot sarakastam?");
+            int skaits = Convert.ToInt32(Console.ReadLine());
+            
+            for(int i = 0; i < skaits; i++)
+            {
+                Console.WriteLine("Ievadiet skaitli.");
+                int input = Convert.ToInt32(Console.ReadLine());
+                numbers.Add(input);
+            }
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            int summa = 0;
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                summa = summa + numbers[i];
+            }
+
+            Console.WriteLine("Skaitlu summa ir " + summa);
+        }
+    }
+}
---
+++ Day12_Lists/Day12_Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day12_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example.task1();
+        }
+    }
+}
