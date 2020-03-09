00bd00163eaaac458e884d0df1ad8547441fa086
Juris <47427449+Breksis@users.noreply.github.com>
2020-03-09 17:58:59 +0200

    Day13

---
+++ Day13_Exceptions/Day13_Exceptions/Exceptions.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Exceptions
+{
+    public class Exceptions
+    {
+        public static void ex1()
+        {
+            Console.WriteLine("Ievadiet skaitli");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Viss ok!");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Nesanaca!");
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
+
+        
+        public static void task2()
+        {
+            //Cilvekam jaievada skaitlis
+            //Japarliecinas, vai tiesam ir ievadits skaitlis
+            //cilveks turpina ievadi, kamer neievada skaitlis
+
+            bool test = false;
+            do
+            {
+                Console.WriteLine("Ievadiet skaitli.");
+                try
+                {
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    test = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Nav ievadits skaitlis.");
+                    Console.WriteLine();
+                }
+            }
+            while (!test);
+        }
+
+        //Lists, kuru aizpilda cilveks - integeri
+        //Uztaist menu
+        // pievienot vertibu
+        //izvadit visu sarakstu
+        //izdzest vertibu pec teksa
+        //izdzest pec indeksa
+        public static void task3()
+        {
+            List<int> saraksts = new List<int>();
+            Console.WriteLine("Cik ierakstus veiksiet?");
+            int skaits = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < skaits; i++)
+            {
+                Console.WriteLine("Ievadiet skaitli");
+                saraksts.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            bool work = true;
+
+            while (work)
+            {
+                Console.WriteLine("1. Pievienot vertibu");
+                Console.WriteLine("2. Izvadit sarakstu");
+                Console.WriteLine("3. Izdzest pec vertibas");
+                Console.WriteLine("4. Izdzest pec indeksa");
+                Console.WriteLine("5. Iziet");
+
+                Console.WriteLine("Kuru darbibu veiksim?");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Ievadiet jauno skaitli.");
+                        try
+                        {
+                            saraksts.Add(Convert.ToInt32(Console.ReadLine()));
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Neizdevas - nav ievadits skaitlis.");
+                        }
+                        break;
+                    case "2":
+                        foreach(int i in saraksts)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        break;
+                    case "3":
+                        foreach (int i in saraksts)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Kuru vertibu dzesisim?");
+                        try
+                        {
+                            saraksts.Remove(Convert.ToInt32(Console.ReadLine()));
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Tada vertiba nav saraksta!");
+                        }
+                        break;
+                    case "4":
+                        for (int i = 0; i < saraksts.Count; i++)
+                        {
+                            Console.WriteLine(i + 1 + ". " + saraksts[i]);
+                        }
+                        Console.WriteLine("Kuru rindu dzesisim?");
+                        int rinda = Convert.ToInt32(Console.ReadLine());
+                        try
+                        {
+                            saraksts.RemoveAt(rinda - 1);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Tada rinda neeksiste!");
+                        }
+
+                        break;
+                    case "5":
+                        work = false;
+                        break;
+                }
+
+            }
+            
+
+
+        }
+    }
+}
---
+++ Day13_Exceptions/Day13_Exceptions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day13_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exceptions.task3();
+            
+        }
+
+        static void task1()
+        {
+            //uztaisit listu ar stringiem
+            //lai cilveks pats aizpilda listu
+            //uztaisit 2 metodes, 1 kas izdzes konkretu saraksta elementu, otru, kas iztira visu sarakstu
+
+            List<string> saraksts = new List<string>();
+            Console.WriteLine("Cik ierakstus veiksiet?");
+            int skaits = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < skaits; i++)
+            {
+                Console.WriteLine("Ievadiet vardu.");
+                saraksts.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine("Vai velaties dzest konkretu ierakstu, vai visu sarakstu? 1 - ierakstu, 2 - sarakstu");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if(choice == 1)
+            {
+                Console.WriteLine("Kuru ierakstu dzest?");
+                int item = Convert.ToInt32(Console.ReadLine());
+                saraksts.RemoveAt(item - 1);
+            }
+            else if(choice == 2)
+            {
+                saraksts.Clear();
+            }
+
+            foreach(string i in saraksts)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
