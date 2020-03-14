d5e5d2f1b705f8a98cf79f5d3961713bfd175df4
Juris <47427449+Breksis@users.noreply.github.com>
2020-03-14 11:45:32 +0200

    Listi

--- Day15_List Reapeat/Day15_List Reapeat/Program.cs
+++ Day15_List Reapeat/Day15_List Reapeat/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Day15_List_Reapeat
 {
@@ -19,12 +20,15 @@ namespace Day15_List_Reapeat
 
             List<int> numbers = new List<int>();
 
+            ReadFile(numbers);
+
             bool turpinat = true;
             while (turpinat)
             {
                 Console.WriteLine("Ievadiet skaitli.");
                 int num = Convert.ToInt32(Console.ReadLine());
                 numbers.Add(num);
+                UpdateList(numbers);
 
                 Console.WriteLine("Vai turpinat ievadit skaitlus?");
                 Console.WriteLine("1 - ja, citi taustini - ne!");
@@ -34,13 +38,80 @@ namespace Day15_List_Reapeat
                     turpinat = true;
                 }
                 else turpinat = false;
+            }
+                bool work = true;
 
+            while (work)
+            {
+                Console.WriteLine("1. Izvadit");
+                Console.WriteLine("2. Pievienot");
+                Console.WriteLine("3. Sasummet");
+                Console.WriteLine("4. Iziet");
 
+                String izvele = Console.ReadLine();
 
-
+                switch (izvele)
+                {
+                    case "1":
+                        foreach (int n in numbers)
+                        {
+                            Console.WriteLine(n);
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Ievadiet skaitli.");
+                        numbers.Add(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case "3":
+                        int sum = 0;
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            sum = sum + numbers[i];
+                        }
+                        Console.WriteLine("Summa ir " + sum);
+                        break;
+                    case "4":
+                        work = false;
+                        break;
+                }
             }
 
+            static void ReadFile(List<int> numbers)
+            {
+                try
+                {
+                    StreamReader read = new StreamReader("C:\\Users\\ronis\\OneDrive\\Documents\\task1.txt");
 
+                    int rinda = Convert.ToInt32(read.ReadLine());
+                    numbers.Add(rinda);
+
+                    while (!read.EndOfStream)
+                    {
+                        numbers.Add(rinda);
+                    }
+                    read.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            static void UpdateList(List<int> numbers)
+            {
+                try
+                {
+                    StreamWriter write = new StreamWriter("C:\\Users\\ronis\\OneDrive\\Documents\\task1.txt");
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        write.WriteLine(numbers[i]);
+                    }
+                    write.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
